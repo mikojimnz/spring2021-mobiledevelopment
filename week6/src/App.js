b8e621bfce99af585bc25686e0653a575146f655
Miko Jimenez <jimenezmikog@gmail.com>
2021-02-15 18:22:32 -0500

    Complette Assignment 3

--- week6/src/App.js
+++ week6/src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+// Import Example Component
+import Example from './components/Example/index';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Output example
+  return <Example/>;
 }
 
 export default App;
---
+++ week6/src/components/Example/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Extend Component
+class Example extends React.Component {
+  render() {
+    // Output Paragraph
+    return '<p>My name is Miko</p>';
+  }
+}
+
+// Export comonent
+export default Example;
