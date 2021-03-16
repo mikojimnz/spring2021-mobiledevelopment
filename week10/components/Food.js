2bec4aa8fae6ba65d33a0c912eb4618740bc811b
Miko Jimenez <jimenezmikog@gmail.com>
2021-03-15 21:50:10 -0400

    Complete Assignment 4

---
+++ week10/components/Food.js
@@ -0,0 +1,14 @@
+import React, {Component} from "React";
+import { Stylesheet, Text, View } from 'react-native';
+
+export default class Name extends Component {
+    render() {
+        return(
+            <View>
+                <Text>
+                    Fettuccine Alfredo Chicken
+                </Text>
+            </View>
+        );
+    }
+}
