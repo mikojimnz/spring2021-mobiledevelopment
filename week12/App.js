ad4741e1b5bbaa41bcbbc945f8b7e8e5fd2c4153
Miko Jimenez <jimenezmikog@gmail.com>
2021-03-29 07:55:36 -0400

    Complete Week 12

--- week12/App.js
+++ week12/App.js
@@ -1,13 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Card, Button } from 'react-native-elements';
 
 export default function App() {
+  const [pet, setPet] = useState();
+
+  const [votes, setVotes] = useState(false);
+
+  function increaseCatVote(value){
+    setPet(value);
+    setVotes(true);
+  }
+
+  function increaseDogVote(value){
+    setPet(value);
+    setVotes(true);
+  }
+
+  function increaseOtherVote(value){
+    setPet(value);
+    setVotes(true);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      { votes ? (
+      <View style={styles.container}>
+        <Text>You voted for {pet}!</Text>
+      </View>
+      ) : (
+        <View>
+        <Text style={styles.headerText}>Vote for your favorite pet!</Text>
+        <Card.Divider/>
+        <Card>
+          <Button title="Cats" onPress={(value) => (increaseCatVote('cats'))}/>
+        </Card>
+        <Card>
+          <Button title="Dogs" onPress={(value) => (increaseDogVote('dogs'))}/>
+        </Card>
+        <Card>
+          <Button title="Other" onPress={(value) => (increaseOtherVote('burgers'))}/>
+        </Card>
+        </View>
+    )
+    }
+     </View>
   );
 }
 
@@ -18,4 +56,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headerText: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+    fontWeight: "bold"
+  }
 });
